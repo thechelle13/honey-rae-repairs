4a08cc23474b9ea17a030e7961a4bc4772f42742
Michelle Totherow <tym4me13@gmail.com>
2023-09-05 14:59:43 -0500

    start of modularizing

--- src/App.js
+++ src/App.js
@@ -24,58 +24,12 @@
 //   </>
 // }
 
-import { useState, useEffect } from "react"
-import { getAllTickets } from "./services/ticketServices.js";
-import './App.css';
+import "./App.css"
+import { TicketLists } from "./components/tickets/TicketLists"
+
 
 export const App = () => {
-  const [allTickets, setAllTickets] = useState([])
-  const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
-  const [filteredTickets, setFilteredTickets] = useState([])
-
-  // useEffect to fetch tickets and set to allTickets on initial render
-
-  useEffect(() => {
-    getAllTickets().then((ticketsArray) => {
-      setAllTickets(ticketsArray)
-      console.log("tickets set")
-    })
-  }, []) 
-
-  useEffect(() => {
-    if (showEmergencyOnly) {
-      const emergencyTickets = allTickets.filter(
-        (ticket) => ticket.emergency === true
-      )
-      setFilteredTickets(emergencyTickets)
-    } else {
-      setFilteredTickets(allTickets)
-    }
-  }, [showEmergencyOnly, allTickets]) // When the dependency contains multiple state variables, the useEffect is watching for any time any of the values change.
-
-  return (
-    <div className="tickets-container">
-      <h2>Tickets</h2>
-      <div>
-        <button className="filter-btn btn-primary" onClick={()=> {setShowEmergencyOnly(true)}}>Emergency</button>
-        <button className="filter-btn  btn-info" onClick={()=> {setShowEmergencyOnly(false)}}>Show All</button>
-      </div>
-      <article className="tickets">
-        {filteredTickets.map((ticket) => {
-          return (
-            <section className="ticket" key={ticket.id}>
-            <header className="ticket-info">#{ticket.id}</header>
-            <div>{ticket.description}</div>
-            <footer>
-              <div>
-                <div className="ticket-info">emergency</div>
-                <div>{ticket.emergency ? "yes" : "no"}</div>
-              </div>
-            </footer>
-            </section>
-          )
-        })}
-        </article>
-        </div>
-)
+  return <>
+  <TicketLists />
+  </>
 }
--- src/services/ticketServices.js
+++ src/services/ticketServices.js
@@ -1,3 +1,3 @@
-export const getAllTickets = () => {
+export const getAllTickets = async() => {
     return fetch(`http://localhost:8088/serviceTickets`).then((res) => res.json())
 }
