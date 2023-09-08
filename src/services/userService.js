a64603dffe2e1f80d8379a1870be8291ad62dd70
Michelle Totherow <tym4me13@gmail.com>
2023-09-08 11:46:04 -0500

    update with auth edits

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,4 +4,22 @@ export const getNonStaffUsers = () => {
     )
 }
 
-//Add function to userService to get staff users
+export const getStaffUsers = () => {
+    return fetch(`http://localhost:8088/users?isStaff=true`).then( (res) => res.json())
+}
+
+export const getUserByEmail = (email) => {
+  return fetch(`http://localhost:8088/users?email=${email}`).then((res) =>
+    res.json()
+  )
+}
+
+export const createUser = (customer) => {
+  return fetch("http://localhost:8088/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(customer),
+  }).then((res) => res.json())
+}
